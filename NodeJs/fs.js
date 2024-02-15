a16c4f7212aeed2ed66f9df5702fb87ea5954661
darshan <darshanchauhan5064@gmail.com>
2024-02-15 17:07:52 +0530

    15 feb

--- NodeJs/fs.js
+++ NodeJs/fs.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
+const fs = require("fs")
 
 const { add, product} = require("./basic.js");
 
 // const {product} = require('./basic.js');
-console.log(add(5,3));
+// console.log(add(5,3));
 // console.log(product(5,3));
 // Synchronously and Asynchronously method
 
 // Asynchronously method
 
-// fs.open('abc.txt',(err, data)=> {
+// fs.open('dc.txt',(err, data)=> {
 //     if(err){
 //         console.log('Filed to open File');
 //     }
@@ -19,8 +19,66 @@ console.log(add(5,3));
 
 // Synchronously method
 
-fs.openSync('abc.txt');
-console.log('File open sucessfully');
+// fs.openSync('dc.txt');
+// console.log('File open sucessfully');
 
 
-// READ method
+
+
+// -------------->>>>>>>>>>>>>>  READ method  <<<<<<<<<<<<<<<<---------------------
+
+// let t1 = performance.now();
+// console.log('product is : ',product(5,3));
+// fs.readFile('dc.txt','utf-8',(err, data)=>{
+//     if(err)
+//       console.log('Failed to read File');
+//      else
+//          console.log(data);
+// });
+
+// let data = fs.readFileSync('dc.txt','utf-8');
+// console.log(data);
+// console.log('Addition is:', add(15,32));
+// let t2 = performance.now();
+// console.log('Time Diffrence is:',t2-t1);
+
+
+
+// -------------->>>>>>>>>>>>>>  WRITE method  <<<<<<<<<<<<<<<<---------------------
+
+// let txt = `#include <stdio.h>
+// int main()
+// {
+//     printf("welcome game of throun");
+// }`;
+
+// fs.writeFileSync("first.c", txt, (err, data)=>{
+//     if (err)
+//      console.log("Failed to write File");
+//     else
+//       console.log("File Write Successful");
+// });
+
+// fs.writeFileSync("hello.c",txt);
+// console.log("File Written sucessfully");
+
+
+// -------------->>>>>>>>>>>>>>  APPEND method  <<<<<<<<<<<<<<<<---------------------
+
+// let txt = `// good morning`;
+// fs.appendFile("basic.c",txt, (err, data)=>{
+//       if (err)
+//        console.log("Failed to write File");
+//       else
+//        console.log("File Write Successful");
+// });
+
+
+// fs.appendFileSync("first.c",txt,(err, data)=>{
+//         console.log("text append successfully");
+// });
+
+
+// -------------->>>>>>>>>>>>>>  CLOSE method  <<<<<<<<<<<<<<<<---------------------
+
+fs.close
