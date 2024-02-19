ea93d6b154f556559e1ac22bc9fff09691c5bcdb
darshan <darshanchauhan5064@gmail.com>
2024-02-19 16:01:55 +0530

    19 feb

--- NodeJs/server2.js
+++ NodeJs/server2.js
@@ -2,14 +2,25 @@
 const http  = require('http');
 
 const fs = require('fs');
-const data = fs.readFileSync("./dc.txt","utf-8");
+// const data = fs.readFileSync("./dc.txt","utf-8");
 
-const server2 = http.createServer((req, res) =>{
-    // res.write('<h1 style"color:pink">welcome to</h1>');
-    // res.setHeader('Content-Type', 'text/html');
-     res.end(data);
-});
+const server2 = http.createServer();
+
+// const server2 = http.createServer((req, res) =>{
+   // res.setHeader('Dummy','Dummy value');
+   // res.setHeader('Content-type','txt/html');
+   //  res.setHeader('content-type','aplication/json');
+   // res.write('{"hello":123}'); 
+   // res.write('<h1 style="color:red">./dc.txt</h1>');
+   // res.end(data);
+// });
   
+   server2.on('request', (req, res) =>{
+      // console.log(req.url);
+      // res.setHeader('Content-Type','txt/html');
+      // res.end(data);
+   })
+
 
    server2.listen(9687, () => {
     console.log('server is running on port 9687');
