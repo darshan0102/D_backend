071c108ba12a5507799db873e4a6ccad64ad6b40
darshan <darshanchauhan5064@gmail.com>
2024-02-24 09:54:43 +0530

    24 feb

---
+++ express/server3.js
@@ -0,0 +1,22 @@
+const express =  require('express');
+const app = express();
+const port = 7070;
+const morgan = require('morgan');
+
+// middleware
+let myFun = (req, res, next) => {
+    if((req.query.age) >=18){
+        next();
+     }
+     else{
+        res.send('you are not 18 years old');
+     }
+}
+
+//  app.use(myFun);   // Application level middleware 
+ app.use(express.json());    // built-in
+ app.use(morgan('dev'));
+
+ app.get('/', (req, res, next)=> {
+    res.
+ })
