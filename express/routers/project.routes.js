69539850d8ba40413d5fd4e82e3ac524b1887937
darshan <darshanchauhan5064@gmail.com>
2024-03-06 18:23:52 +0530

    6 mar

--- express/routers/project.routes.js
+++ express/routers/project.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const ProjectRoutes = express.Router();
 
-const {addProject} = require('../controller/project.controll');
+const {addProject, getAllProject} = require('../controller/project.controll');
 
 ProjectRoutes.post('/add-Project', addProject);
+ProjectRoutes.get('/get-all-Project', getAllProject);
+// ProjectRoutes.get('/get-Project', getProject);
 
 module.exports = ProjectRoutes;
