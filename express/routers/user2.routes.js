00ff71ee35451ae675fd337781f8a9803e3b88d3
darshan <darshanchauhan5064@gmail.com>
2024-03-12 16:15:29 +0530

    12 mar

--- express/routers/user2.routes.js
+++ express/routers/user2.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const userRoutes = express.Router();
-const {addUser,getAllUsers,getUser} = require('../controller/user2.controll');
+const {registerUser,
+       loginUser, 
+       addUser,
+       getAllUsers,
+       getUser} = require('../controller/user2.controll');
 
-userRoutes.post('/add-user', addUser);
-userRoutes.get('/get-all-users', getAllUsers);
-userRoutes.get('/get-user', getUser);
+userRoutes.post('/register-user',registerUser);
+userRoutes.post('/login-user',loginUser);
+// userRoutes.post('/add-user', addUser);
+// userRoutes.get('/get-all-users', getAllUsers);
+// userRoutes.get('/get-user', getUser);
 
-module.exports = userRoutes; 
+module.exports = userRoutes;    
